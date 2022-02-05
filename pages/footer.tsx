a8546b22851a306578bb42b5a656c7eb32e5a92b
MINYEONG KIM <doingmyk@gmail.com>
2022-02-05 14:35:58 +1100

    create footer file

---
+++ pages/footer.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Footer: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      This is Footer
+    </div>
+  )
+}
+
+export default Footer
