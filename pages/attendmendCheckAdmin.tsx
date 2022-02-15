4e19b538cb4cc5d16ab3f6084bf85d7fd7b6e230
LGE0131 <igoeun0131@gamil.com>
2022-02-15 17:42:59 +0900

    출퇴근 기록 페이지 초안 추가

---
+++ pages/attendmendCheckAdmin.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TabList from "../components/TabList";
+
+const attendmendCheckAdmin = () => {
+  return (
+    <Box>
+      <Typography
+        variant="h3"
+        sx={{
+          padding: "0 0 1rem 0",
+        }}
+      >
+        출퇴근 기록
+      </Typography>
+      <Box>
+        <TabList />
+      </Box>
+    </Box>
+  );
+};
+
+export default attendmendCheckAdmin;
