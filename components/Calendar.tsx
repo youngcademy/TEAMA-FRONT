1be4e909779f60e2ee3992d1d6fe800344fccf82
LGE0131 <igoeun0131@gamil.com>
2022-02-22 18:39:31 +0900

    add react-big-calendar but error

---
+++ components/Calendar.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Calendar, View, DateLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import { momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const allViews: View[] = ["agenda", "day", "week", "month"];
+
+interface Props {
+  localizer: DateLocalizer;
+}
+
+class CalendarEvent {
+  title: string;
+  allDay: boolean;
+  start: Date;
+  end: Date;
+  desc: string;
+  resourceId?: string;
+  tooltip?: string;
+
+  constructor(
+    _title: string,
+    _start: Date,
+    _endDate: Date,
+    _allDay?: boolean,
+    _desc?: string,
+    _resourceId?: string
+  ) {
+    this.title = _title;
+    this.allDay = _allDay || false;
+    this.start = _start;
+    this.end = _endDate;
+    this.desc = _desc || "";
+    this.resourceId = _resourceId;
+  }
+}
+
+function SelectableCalendar({ localizer }: Props) {
+  const [events, setEvents] = useState([
+    { start: moment(), end: moment().add(1, "hours"), title: "today" },
+  ] as CalendarEvent[]);
+
+  const handleSelect = ({ start, end }: any) => {
+    const title = window.prompt("New Event name");
+
+    if (title) {
+      let newEvent = {} as CalendarEvent;
+      newEvent.start = moment(start).toDate();
+      newEvent.end = moment(end).toDate();
+      newEvent.title = title;
+
+      // Erroneous code
+      // events.push(newEvent)
+      // setEvents(events)
+      setEvents([...events, newEvent]);
+    }
+  };
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getDay();
+  const day = today.getDate();
+
+  return (
+    <>
+      <Calendar
+        selectable
+        localizer={localizer}
+        events={events}
+        defaultView="month"
+        views={allViews}
+        defaultDate={today}
+        onSelectEvent={(event) => alert(event.title)}
+        onSelectSlot={handleSelect}
+        startAccessor="start"
+        endAccessor="end"
+        titleAccessor="title"
+      />
+    </>
+  );
+}
+
+export default function Availability() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <SelectableCalendar localizer={localizer} />
+    </div>
+  );
+}
---
+++ pages/admin/employlist.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const employlist = () => {
+  return <div>employlist</div>;
+};
+
+export default employlist;
