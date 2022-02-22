2d154d51ed159ec8c45c86a8c090e66622332e81
ldhee1103 <ldhee1103@naver.com>
2022-02-22 20:51:55 +0900

    scss위치 변경 및 Page에 적용

--- pages/Position/Position.tsx
+++ pages/Position/Position.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "../styles/Position.module.scss";
 
 function Position() {
-  return <div>Position Page!</div>;
+  return (
+    <div>
+      Position
+    </div>
+  )
 }
 
 export default Position;
