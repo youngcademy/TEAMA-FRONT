c50a9d5515544298e1d4de5b2264b5e282f441b8
ldhee1103 <ldhee1103@naver.com>
2022-02-25 22:36:50 +0900

    MUI Grid 적용

--- pages/Position/Position.tsx
+++ pages/Position/Position.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { columns } from "../../components/grid/position/columns";
 import { rows } from "../../components/grid/position/row";
+import { Grid, Box } from "@material-ui/core";
 import styles from "../styles/Position.module.scss";
 
 function Position() {
@@ -10,6 +11,43 @@ function Position() {
 
   return (
     <div style={{ height: 400, width: "100%" }}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Box bgcolor="info.main" p={2}>
+            1
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor="info.main" p={2}>
+            2
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor="info.main" p={2}>
+            3
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Box bgcolor="info.main" color="info.contrastText" p={2}>
+            검색구분
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Box bgcolor="info.main" color="info.contrastText" p={2}>
+            검색어
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Box bgcolor="info.main" color="info.contrastText" p={2}>
+            검색버튼
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Box bgcolor="info.main" color="info.contrastText" p={2}>
+            수정/삭제 버튼
+          </Box>
+        </Grid>
+      </Grid>
       <DataGrid
         rows={rowData}
         columns={columnData}
