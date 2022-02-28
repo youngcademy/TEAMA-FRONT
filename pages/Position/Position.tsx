44798fed977e633c535dbfdf3c17c4c62f248f46
ldhee1103 <ldhee1103@naver.com>
2022-02-28 22:03:52 +0900

    MUI TREEVIEW 적용

--- pages/Position/Position.tsx
+++ pages/Position/Position.tsx
@@ -3,6 +3,11 @@ import { DataGrid } from "@mui/x-data-grid";
 import { columns } from "../../components/grid/position/columns";
 import { rows } from "../../components/grid/position/row";
 import { Grid, Box } from "@material-ui/core";
+import Button from "@mui/material/Button";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TreeView from "@mui/lab/TreeView";
+import TreeItem from "@mui/lab/TreeItem";
 import styles from "../styles/Position.module.scss";
 
 function Position() {
@@ -34,7 +39,7 @@ function Position() {
         </Grid>
         <Grid item xs={6} sm={3}>
           <Box bgcolor="info.main" color="info.contrastText" p={2}>
-            검색어
+            cd 검색어
           </Box>
         </Grid>
         <Grid item xs={6} sm={3}>
@@ -57,8 +62,24 @@ function Position() {
         disableSelectionOnClick
         isRowSelectable={(params: any) => params.row.OrderAmount > 1}
       />
+      <TreeView
+        aria-label="file system navigator"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+      >
+        <TreeItem nodeId="1" label="Applications">
+          <TreeItem nodeId="2" label="Calendar" />
+        </TreeItem>
+        <TreeItem nodeId="5" label="Documents">
+          <TreeItem nodeId="10" label="OSS" />
+          <TreeItem nodeId="6" label="MUI">
+            <TreeItem nodeId="8" label="index.js" />
+          </TreeItem>
+        </TreeItem>
+      </TreeView>
     </div>
-  )
+  );
 }
 
 export default Position;
