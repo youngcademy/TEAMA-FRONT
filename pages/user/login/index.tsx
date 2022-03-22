213d6eda5b3222c68a9831f636a33ebeb37d16ef
byung ju choi <bjc30849@gmail.com>
2022-03-22 21:17:09 +0900

    feat: axios login

--- pages/user/login/index.tsx
+++ pages/user/login/index.tsx
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 interface IUsersProps {
+  users: IUserProps[];
+}
+
+interface IUserProps {
+  id: number;
   name: string;
+  username: string;
   email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 }
 
 const Login = ({ users }: IUsersProps) => {
-  return <div>Login</div>;
+  const [Id, setID] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onChangeId = (e: React.FormEvent<HTMLInputElement>) => {
+    setID(e.currentTarget.value);
+  };
+  const onChangePW = (e: React.FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmit = () => {
+    console.log(Id, Password);
+  };
+  return (
+    <div>
+      {users.map((users, index) => (
+        <h3 key={index}>{users.name}</h3>
+      ))}
+      <input onChange={onChangeId} value={Id} />
+      <input type={"password"} onChange={onChangePW} value={Password} />
+      <button onClick={onSubmit}>전송</button>
+    </div>
+  );
 };
 
-Login.getInitalProps = async () => {
+Login.getInitialProps = async () => {
   const { data: users } = await axios.get(
     "https://jsonplaceholder.typicode.com/users"
   );
-  console.log("data loaded");
+  console.log(users);
   return { users };
 };
 
